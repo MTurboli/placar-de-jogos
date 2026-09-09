c79aa531218558db94c061b30d3bab9932c8f226
MTurboli <mathturboli@gmail.com>
2026-09-09 17:32:20 -0300

    interação-botões

--- script.js
+++ script.js
@@ -13,16 +13,39 @@ playerForm.addEventListener('submit', function(event){
     }
 
     const cardConteiner = document.createElement('div')
+
     cardConteiner.classList.add('player-card')
+
     cardConteiner.innerHTML = `
                                <h3>${playerName}</h3>
-                               <button class="btn-plus-minus">+1</button>
+                               <button class="btn-plus">+1</button>
                                <span>0</span>
-                               <button class="btn-plus-minus">-1</button>
+                               <button class="btn-minus">-1</button>
                                `
     
     containerPlayerList.appendChild(cardConteiner)
 
     formInputPlayerName.value = ''
+    
     formInputPlayerName.focus()
+})
+
+containerPlayerList.addEventListener('click', function(event) {
+    if (event.target.classList.contains('btn-plus')) {
+        const clickBtnPlus = event.target.closest('.player-card')
+
+        const scoreSpan = clickBtnPlus.querySelector('span')
+
+        const currentScore = Number(scoreSpan.textContent)
+
+        scoreSpan.textContent = currentScore + 1
+    } else if (event.target.classList.contains('btn-minus')) {
+        const clickBtnMinus = event.target.closest('.player-card')
+
+        const scoreSpan = clickBtnMinus.querySelector('span')
+
+        const currentScore = Number(scoreSpan.textContent)
+
+        scoreSpan.textContent = currentScore - 1
+    }
 })
